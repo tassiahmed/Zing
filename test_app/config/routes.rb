b1d3e11406965331dcb66023a5ffe136a0267c75
Phil LaGambino <plagam95@gmail.com>
2016-11-06 16:15:47 -0500

    The file uploading capability is active for various basic documents. This code will soon be implemented with the master branch and the specific database that has been constructed for the final database.

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  get 'reumes/index'
-
-  get 'reumes/new'
-
-  get 'reumes/create'
-
-  get 'reumes/destroy'
-
-  get 'resumes/indesx'
-
-  get 'resumes/new'
-
-  get 'resumes/create'
-
-  get 'resumes/destroy'
-
+    resources :resumes, only: [:index, :new, :create, :destroy]
+    root 'resumes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
