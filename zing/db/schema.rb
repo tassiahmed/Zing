687288a544ab99cd40f4a86c4b834a61926075db
Evan Thompson <evanmt13@gmail.com>
2016-11-06 15:30:56 -0500

    Adds hashing for the file name

--- zing/db/schema.rb
+++ zing/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001185926) do
+ActiveRecord::Schema.define(version: 20161106194120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161001185926) do
     t.string   "file_url"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.index ["file_url"], name: "index_files_on_file_url", unique: true, using: :btree
   end
 
   add_foreign_key "file_datas", "files"
