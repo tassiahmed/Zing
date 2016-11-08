c725c0f4f9dba0f8734e0eab1d8890c47c2554a5
Tausif Ahmed <atausif00@gmail.com>
2016-11-08 16:40:11 -0500

    Started cleaning up the routes

--- zing/config/routes.rb
+++ zing/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :documents, only: [:index, :new, :create]
-  get 'document', to: 'documents#download'
+  # resources :documents, only: [:index, :new, :create]
+  # get 'document', to: 'documents#download'
 
-  root to: redirect('/documents')
+  resources :documents do
+    member do
+      get :download, param: :file_url
+    end
+  end
+
+  # root to: redirect('/documents')
 end
