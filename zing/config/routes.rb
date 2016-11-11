7b1835f8a0c5e6de4434d549fb21fdb32bc11209
Tausif Ahmed <atausif00@gmail.com>
2016-11-11 11:49:34 -0500

    Added storing of passwords

--- zing/config/routes.rb
+++ zing/config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :documents, only: [:index, :new, :create]
-  # get 'document', to: 'documents#download'
-
   resources :documents do
     member do
       get :download, param: :file_url
-      get :show, param: :file_url
+      post :show, param: :file_url
     end
   end
 
