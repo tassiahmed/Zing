145db6f3cc0c7f9adc24f7f9220134fbc4998145
Evan Thompson <evanmt13@gmail.com>
2016-11-29 16:53:50 -0500

    Added CLI input for port

--- FileSystem/src/fmServer.c
+++ FileSystem/src/fmServer.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("Hello world\n");
-    printf("Test!\n");
+#define PORT_STR "-p"
+
+
+struct inputs{
+    int port;
+};
+
+struct inputs constructParams(int argc, char** args){
+    struct inputs params = { 0 };
+    for(int i = 0; i < argc; i++){
+        if(strcmp(args[i], PORT_STR) == 0){
+            i++;
+            params.port = atoi(args[i]);
+        }
+    }
+    return params;
+}
+
+int main(int argc, char** args){
+    struct inputs params = constructParams(argc, args);
+    printf(" port: %d\n", params.port);
 }
